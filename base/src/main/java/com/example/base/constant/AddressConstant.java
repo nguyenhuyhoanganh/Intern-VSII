ba8b8c0dd90fe23eaa7262eccf85ac8824c9ec88
Oanh <oanh.np.2003@gmail.com>
2024-01-06 20:01:02 +0700

    update

--- base/src/main/java/com/example/base/constant/AddressConstant.java
+++ base/src/main/java/com/example/base/constant/AddressConstant.java
@@ -5,8 +5,8 @@ package com.example.base.constant;
  */
 public class AddressConstant {
     public static final String LINE_NOT_BLANK = "Tên đường không được để trống";
-    public static final String DISTRICT_NOT_BLANK = "Tên đường không được để trống";
-    public static final String PROVINCE_NOT_BLANK = "Tên đường không được để trống";
-    public static final String WARD_NOT_BLANK = "Tên đường không được để trống";
+    public static final String WARD_NOT_BLANK = "Tên xã/phường không được để trống";
+    public static final String DISTRICT_NOT_BLANK = "Tên quận/huyện không được để trống";
+    public static final String PROVINCE_NOT_BLANK = "Tên tỉnh/thành phố không được để trống";
 
 }
