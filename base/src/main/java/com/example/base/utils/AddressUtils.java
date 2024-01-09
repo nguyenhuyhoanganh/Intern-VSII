839d934be68cfe5b98b09c38ec87fd26853f1648
Oanh <oanh.np.2003@gmail.com>
2024-01-09 08:08:37 +0700

    update

--- base/src/main/java/com/example/base/utils/AddressUtils.java
+++ base/src/main/java/com/example/base/utils/AddressUtils.java
@@ -21,14 +21,7 @@ public class AddressUtils {
      * @return Đối tượng Address được chuyển đổi.
      */
     public Address mapAddressDtoToAddress(AddressDTO addressDTO){
-        Address address = modelMapper.map(addressDTO, Address.class);
-
-        if (addressDTO.getUserId() != null) {
-            User user = modelMapper.map(addressDTO.getUserId(), User.class);
-            address.setUser(user);
-        }
-        return address;
-//        return modelMapper.map(addressDTO,Address.class);
+        return modelMapper.map(addressDTO,Address.class);
     }
     /**
      * Chuyển đổi đối tượng Address thành  AddressDTO
@@ -36,13 +29,6 @@ public class AddressUtils {
      * @return Đối tượng AddressDTO được chuyển đổi.
      */
     public AddressDTO mapAddressToAddressDto(Address address) {
-        AddressDTO addressDTO = modelMapper.map(address, AddressDTO.class);
-
-        if (address.getUser() != null) {
-            addressDTO.setUserId(address.getUser().getId());
-        }
-        return addressDTO;
+        return modelMapper.map(address, AddressDTO.class);
     }
-//        return modelMapper.map(address, AddressDTO.class);
-//    }
 }
