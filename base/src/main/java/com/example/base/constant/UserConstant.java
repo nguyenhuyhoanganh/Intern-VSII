2ee5ec532851d719edf3788f5e4c7ff0e4926c67
Thang280602 <thang280602@gmail.com>
2024-01-18 10:22:34 +0700

    Update final message and utf-8

--- base/src/main/java/com/example/base/constant/UserConstant.java
+++ base/src/main/java/com/example/base/constant/UserConstant.java
@@ -12,23 +12,29 @@ public class UserConstant {
     private static MessageSource messageSource;
 
     public static String AGE_NOT_VALID;
+    public static String USER_MESSAGE_IS_NULL;
+    public static String USER_NOT_FOUND_BY_USERNAME;
 
     public UserConstant(MessageSource messageSource) {
         UserConstant.messageSource = messageSource;
         AGE_NOT_VALID = messageSource.getMessage("validation.user.age.not.under.18", null, Locale.getDefault());
+        USER_MESSAGE_IS_NULL = messageSource.getMessage("exception.user.id.not_valid", null, Locale.getDefault());
+        USER_NOT_FOUND_BY_USERNAME = messageSource.getMessage("exception.user.not_found_by_username", null, Locale.getDefault());
+
+
     }
     public static final String FIRST_NAME_NOT_BLANK = "validation.user.first_name.not_blank";
     public static final String BLANK = "";
     public static final String EMAIL_NOT_VALID = "validation.user.email.not_valid";
     public static final String PHONE_NUMBER_NOT_BLANK = "validation.user.phone_number.not_blank";
     public static final String DATE_OF_BIRTH_NOT_VALID = "validation.user.date_of_birth.not_valid";
-    public static final String USER_MESSAGE_NOT_FOUND = "Người dùng không tồn tại";
+    public static final String USER_MESSAGE_NOT_FOUND = "exception.user.not_found";
     public static final String ROLE_NOT_FOUND = "Role không tồn tại";
-    public static final String USER_MESSAGE_IS_NULL = "exception.user.id.not_valid";
-    public static final String USER_MESSAGE_UPDATE_FAIL = "exception.user.update_fail";
-    public static final String USER_NOT_FOUND_BY_USERNAME = "exception.user.not_found_by_username";
 
-    public static final String USER_NOT_FOUND_BY_ID="exception.user.id.not_valid";
+//    public static final String USER_MESSAGE_UPDATE_FAIL = "exception.user.update_fail";
+
+
+//    public static final String USER_NOT_FOUND_BY_ID="exception.user.id.not_valid";
 
     public static final String USERNAME_NOT_BLANK = "validation.user.username.not_blank";
 
